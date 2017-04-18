c4a7d1ef7cb2608cc27e867527adc1dd369db5e2
Mike Taylor <20064376@mail.wit.ie>
2017-04-18 20:52:10 +0100

    added in boilerplate code for the portfolio website, added document work to reflect that

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,14 @@ gulp.task('nunjucks', function() {
   // output files in app folder
   .pipe(gulp.dest('boilerplate/blog'));
 });
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('boilerplate/portfolio/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['boilerplate/portfolio/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('boilerplate/portfolio'));
+});
