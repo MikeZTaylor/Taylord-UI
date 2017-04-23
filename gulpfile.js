4fbfdda7f0f36ee7595fc02281983ffb0468d946
Mike Taylor <20064376@mail.wit.ie>
2017-04-23 23:11:54 +0100

    cleaning up project files

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,35 @@ var nunjucksRender = require('gulp-nunjucks-render');
 
 gulp.task('nunjucks_one', function() {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('boilerplate/blog/pages/**/*.+(html|nunjucks)')
+  return gulp.src('templates/blog/pages/**/*.+(html|nunjucks)')
   // Renders template with nunjucks
   .pipe(nunjucksRender({
-      path: ['boilerplate/blog/templates']
+      path: ['templates/blog/templates']
     }))
   // output files in app folder
-  .pipe(gulp.dest('boilerplate/blog'));
+  .pipe(gulp.dest('templates/blog'));
 });
 
 gulp.task('nunjucks_two', function() {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('boilerplate/portfolio/pages/**/*.+(html|nunjucks)')
+  return gulp.src('templates/portfolio/pages/**/*.+(html|nunjucks)')
   // Renders template with nunjucks
   .pipe(nunjucksRender({
-      path: ['boilerplate/portfolio/templates']
+      path: ['templates/portfolio/templates']
     }))
   // output files in app folder
-  .pipe(gulp.dest('boilerplate/portfolio'));
+  .pipe(gulp.dest('templates/portfolio'));
 });
 
 gulp.task('nunjucks_three', function() {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('boilerplate/product/pages/**/*.+(html|nunjucks)')
+  return gulp.src('templates/product/pages/**/*.+(html|nunjucks)')
   // Renders template with nunjucks
   .pipe(nunjucksRender({
-      path: ['boilerplate/product/templates']
+      path: ['templates/product/templates']
     }))
   // output files in app folder
-  .pipe(gulp.dest('boilerplate/product'));
+  .pipe(gulp.dest('templates/product'));
 });
 
 
